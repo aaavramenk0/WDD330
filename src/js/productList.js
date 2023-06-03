38ac265354dfae0e8eef6cf9088f24f16f001f9d
Aleksandr <alex.avramenko2004@gmail.com>
2023-06-03 11:31:34 -0400

    Week 7

--- src/js/productList.js
+++ src/js/productList.js
@@ -1,11 +1,11 @@
-import { getData } from "./productData.mjs";
+import { getProductsByCategory } from "./externalServices.mjs";
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
   return `<li class="product-card">
-    <a href="product_pages/index.html?product=${product.Id}">
+    <a href="/product_pages/index.html?product=${product.Id}">
     <img
-      src="${product.Image}"
+      src="${product.Images.PrimaryMedium}"
       alt="Image of ${product.Name}"
     />
     <h3 class="card__brand">${product.Brand.Name}</h3>
@@ -18,8 +18,9 @@ export default async function productList(selector, category) {
   // get the element we will insert the list into from the selector
   const el = document.querySelector(selector);
   // get the list of products
-  const products = await getData(category);
+  const products = await getProductsByCategory(category);
   console.log(products);
   // render out the product list to the element
   renderListWithTemplate(productCardTemplate, el, products);
+  document.querySelector(".title").innerHTML = category;
 }
